a089d808ae589aef12cc4cbd2171c29dac69403c
gavan <guoming3@xiaomi.com>
2023-04-28 14:33:25 +0800

    misc.

--- src/components/SelectLimitDoc.vue
+++ src/components/SelectLimitDoc.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
-    <ex-select-limit style="width: 400px;margin: 10px 0;"
-      v-model="selectedItem"
-      placeholder="默认 limit = 30，实际有10000条数据"
-      :options="options"
-      :limit="limit"
-      @change="handleChange"
+    <el-form
+      :model="selectedItem"
+      label-width="100px"
+      label-position="left"
+      ref="form"
     >
-    </ex-select-limit>
+      <el-form-item label="关联应用" prop="milogAppId">
+        <ex-select-limit
+          style="width: 400px; margin: 10px 0"
+          v-model="selectedItem.milogAppId"
+          placeholder="默认 limit = 30，实际有10000条数据"
+          :options="options"
+          :limit="limit"
+          @change="handleChange"
+        >
+        </ex-select-limit>
+      </el-form-item>
+    </el-form>
     <div>
       <span>切换limit：</span>
-      <el-select v-model="limit">  
-        <el-option v-for="item in limits" :key="item" :label="item" :value="item">
+      <el-select v-model="limit">
+        <el-option
+          v-for="item in limits"
+          :key="item"
+          :label="item"
+          :value="item"
+        >
         </el-option>
       </el-select>
     </div>
@@ -19,34 +34,36 @@
 </template>
 
 <script>
-
 const fakeData = () => {
   const options = [];
   for (let i = 1; i <= 10000; i++) {
     options.push({
       value: i,
       label: "label " + i,
-      other: 'xxxx',
+      other: "xxxx",
     });
   }
   return options;
-}
+};
 export default {
   data() {
     return {
       limit: 30,
       limits: [5, 30, 100, 500, 800, 1000, 2000, 3000, 5000, 10000],
       options: [],
-      selectedItem: -1,
+      selectedItem: {},
     };
   },
   mounted() {
     setTimeout(() => {
-      this.options = fakeData()
-      setTimeout(() => {
-        this.selectedItem = 2
-      }, 1000)
-    }, 3000)
+      this.selectedItem = {
+        ...this.selectedItem,
+        milogAppId: 2,
+      };
+    }, 100);
+    setTimeout(() => {
+      this.options = fakeData();
+    }, 3000);
   },
   methods: {
     filterItemDemox(item, query) {
@@ -54,8 +71,8 @@ export default {
       return true;
     },
     handleChange() {
-      console.log('handleChange', this.selectedItem)
-    }
+      console.log("handleChange", this.selectedItem);
+    },
   },
   watch: {
     selectedItem(value) {
@@ -64,5 +81,4 @@ export default {
   },
 };
 </script>
-<style>
-</style>
+<style></style>
